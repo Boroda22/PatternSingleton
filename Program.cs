5e959747b50200d78f2678334ba17c3bf8e50f48
Александр <56580845+Boroda22@users.noreply.github.com>
2020-12-12 19:49:07 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternSingleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // пример использования одиночки
+            Singleton singleton1 = Singleton.GetInstance();
+            Singleton singleton2 = Singleton.GetInstance();
+
+            if(singleton1 == singleton2)
+            {
+                Console.WriteLine("Переменные содержат один и тот же экземпляр");
+            }
+            else
+            {
+                Console.WriteLine("Переменные содержат разные экземпляры");
+            }
+        }
+    }
+}
